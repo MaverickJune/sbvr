225b6b4cf4590bdac45eb015dc340d12bd61f6e1
Maverick_June <wjbang@snu.ac.kr>
2025-05-03 12:57:24 +0900

    automated acc measurement

--- eval/measure_zero_shot_acc.py
+++ eval/measure_zero_shot_acc.py
@@ -3,6 +3,9 @@ from lm_eval import evaluator
 from transformers import AutoTokenizer, LlamaForCausalLM
 from sbvr_utils.utils_llama import sbvr_decompress_on_llama, get_llama
 from sbvr_utils.lm_eval_adaptor import LMEvalAdaptor
+from pathlib import Path
+import argparse
+import os
 
 def get_model_and_enc(model_path, use_sbvr=False, use_llm_int8=False, use_fp8=False, 
                       use_gptq_4=False, use_awq_4=False, gptq_local_model_path:str = None,
@@ -26,15 +29,18 @@ def get_model_and_enc(model_path, use_sbvr=False, use_llm_int8=False, use_fp8=Fa
 
 def measure_zero_shot_reasoning_task(model_path=None,
                                      sbvr_path=None,
-                                     tasks=None):
-    if None in [model_path, sbvr_path, tasks]:
-        raise ValueError("model_path, sbvr_path and tasks cannot be None")
+                                     tasks=None,
+                                     log_file_name=None,
+                                     use_llm_int8=False,):
+    if None in [model_path, tasks]:
+        raise ValueError("model_path and tasks cannot be None")
     
     # Load the SBVR model
     model, tokenizer = get_model_and_enc(model_path=model_path, 
-                                         use_sbvr=True,
+                                         use_sbvr=(sbvr_path is not None),
                                          weight_path=model_path,
-                                         sbvr_from_local_path=sbvr_path)
+                                         sbvr_from_local_path=sbvr_path,
+                                         use_llm_int8=use_llm_int8)
     
     # Create an evaluator
     lm_eval_model = LMEvalAdaptor(model_name=model_path, model=model, tokenizer=tokenizer, batch_size=1)
@@ -47,12 +53,24 @@ def measure_zero_shot_reasoning_task(model_path=None,
         no_cache=True,
         num_fewshot=0,
     )
-
-    print(evaluator.make_table(results))
+    table = evaluator.make_table(results)
+    print(table)
+    output_dir = Path(__file__).parent / "results"
+    os.makedirs(output_dir, exist_ok=True)
+    output_path = os.path.join(output_dir, f"{log_file_name}.txt")
+    with open(output_path, "w") as f:
+        f.write(table)
+    print(f"Results saved to {output_path}")
     
 if __name__ == "__main__":
-    MODEL_PATH = "meta-llama/Llama-3.1-8B"
-    SBVR_PATH = "/home/nxclab/wonjun/bvq/sbvr_models/meta-llama_Llama-3.1-8B_num_sum_4"
+    parser = argparse.ArgumentParser(description="Measure zero-shot reasoning task performance")
+    parser.add_argument("--model_path", type=str, required=True, help="Path to the model", default=None)
+    parser.add_argument("--sbvr_path", type=str, help="Path to the SBVR model", default=None)
+    parser.add_argument("--use_llm_int8", action="store_true", help="Use LLM.int8 quantization")
+    parser.add_argument("--log_file_name", type=str, help="Name of the result txt file", default=None)
+    
+    args = parser.parse_args()
+    
     TASKS = ["arc_easy", 
              # "arc_challenge", 
              "boolq", 
@@ -62,7 +80,10 @@ if __name__ == "__main__":
              # "sciq", 
              "winogrande"]
     
-    measure_zero_shot_reasoning_task(model_path=MODEL_PATH, sbvr_path=SBVR_PATH, tasks=TASKS)
+    measure_zero_shot_reasoning_task(model_path=args.model_path, 
+                                     sbvr_path=args.sbvr_path, 
+                                     tasks=TASKS,
+                                     use_llm_int8=args.use_llm_int8)
 
 # MODEL_PATH = "meta-llama/Llama-3.2-1B"
 # WEIGHT_PATH = "/home/nxc/sbvr/compressed_weights" 
